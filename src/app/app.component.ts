5c38a8c56d4a0962137eae77387879ebf81bc293
khaled charbti <charbtikhaled001@gmail.com>
2025-02-03 12:57:42 +0100

    LINKs2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,8 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  constructor() {
+    injectSpeedInsights(); // Initialize SpeedInsights
+  }
   title = 'Memoriesmenu';
 }
